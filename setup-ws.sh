ed0f0e8d09786469525dedad27c1802a846820f2
hengsin <hengsin@penguin>
2020-01-19 16:46:44 +0800

    add support to clone release branch. add command line options support. add validation for db credentials

---
+++ setup-ws.sh
@@ -0,0 +1,62 @@
+TYCHO_REPOSITORY=https://repo1.maven.org/maven2/.m2e/connectors/m2eclipse-tycho/0.8.1/N/0.8.1.201704211436/
+CDT_REPOSITORY=https://download.eclipse.org/tools/cdt/releases/9.10/
+ECLIPSE=${ECLIPSE:-eclipse}
+IDEMPIERE_SOURCE_FOLDER=${IDEMPIERE_SOURCE_FOLDER:-idempiere}
+
+for i in "$@"
+do
+    case $i in
+    --source=*)
+    IDEMPIERE_SOURCE_FOLDER="${i#*=}"
+    shift # past argument=value
+    ;;
+    --eclipse=*)
+    ECLIPSE="${i#*=}"
+    shift # past argument=value
+    ;;
+    --help)
+    echo "Usage: setup-ws.sh [OPTION]"
+    echo ""
+    echo -e "  --eclipse=<eclipse ide folder>"
+    echo -e "\tSet eclipse ide folder (default is eclipse)"
+    echo -e "  --source=<idempiere source folder>"
+    echo -e "\tSet idempiere source folder (default is idempiere)"
+    echo -e "  --help"
+    echo -e "\tdisplay this help and exit"
+    exit 0
+    ;;
+    *)
+	shift
+	;;
+    esac
+done
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [[ $IDEMPIERE_SOURCE_FOLDER == "/*" ]]; then
+	:
+else
+	IDEMPIERE_SOURCE_FOLDER="$DIR/$IDEMPIERE_SOURCE_FOLDER"	
+fi
+
+cd $ECLIPSE
+DESTINATION=$(pwd)
+
+echo "** Install Tycho Configurator ..."
+./eclipse -nosplash -data "$IDEMPIERE_SOURCE_FOLDER" -application org.eclipse.equinox.p2.director \
+	-repository $TYCHO_REPOSITORY -destination "$DESTINATION" \
+	-installIU org.sonatype.tycho.m2e.feature.feature.group
+
+echo "** Install CDT Headless Build ..."
+./eclipse -nosplash -data $IDEMPIERE_SOURCE_FOLDER -application org.eclipse.equinox.p2.director \
+	-repository $CDT_REPOSITORY -destination "$DESTINATION" \
+	-installIU org.eclipse.cdt.managedbuilder.core
+
+./eclipse -nosplash -data "$IDEMPIERE_SOURCE_FOLDER" -application org.eclipse.ant.core.antRunner -buildfile "$DIR/setup-ws.xml" -Didempiere="$IDEMPIERE_SOURCE_FOLDER"
+
+echo "** Build workspace ..."
+./eclipse -nosplash \
+	-application org.eclipse.cdt.managedbuilder.core.headlessbuild \
+	-data "$IDEMPIERE_SOURCE_FOLDER" \
+	-build all
+
+cd "$DIR"
