6a96d250f48ba45484f9d6604f2ebf7e741b743c
muriloht <muriloht@devcoffee.com.br>
2025-04-11 14:29:11 -0300

    * fix bash completion

--- completion.bash
+++ completion.bash
@@ -1,6 +1,7 @@
 #/usr/bin/env bash
 complete -W "--branch --db-admin-pass --db-host --db-name --db-pass --db-port --db-user --docker-postgres-create --docker-postgres-name --docker-postgres-version --eclipse /
---help --http-host --http-port --https-port --load-idempiere-env --repository-url --skip-migration-script --skip-setup-db --source" ./setup.sh
+--help --http-host --http-port --https-port --load-idempiere-env --repository-url --skip-migration-script --skip-setup-db --source --install-copilot" ./setup.sh
 complete -W "--db-admin-pass --db-port --docker-postgres-name --docker-postgres-version" ./docker-postgres.sh
 complete -W "--db-admin-pass --db-host --db-name --db-pass --db-port --db-user /
 --help --http-host --http-port --https-port --run-migration-script --source" ./setup-db.sh
+complete -W "--eclipse --source --help --install-copilot" ./setup-ws.sh
